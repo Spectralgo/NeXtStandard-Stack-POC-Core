6e19831264b65991f49422d6fcc32651a95175c6
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 01:40:41 +0200

    BROKERS: Logging - Log LogWarning

--- NeXtStandardStack.Core.Api/Brokers/Loggings/LoggingBroker.cs
+++ NeXtStandardStack.Core.Api/Brokers/Loggings/LoggingBroker.cs
@@ -18,5 +18,8 @@ namespace NeXtStandardStack.Core.Api.Brokers.Loggings
 
         public void LogDebug(string message) =>
             this.logger.LogDebug(message);
+
+        public void LogWarning(string message) =>
+            this.logger.LogWarning(message);
     }
 }
