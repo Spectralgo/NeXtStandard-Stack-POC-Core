4618338489d688cdac573249cb364f330ae53c31
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 01:51:15 +0200

    BROKERS: Storage - Select Player By Id

--- NeXtStandardStack.Core.Api/Brokers/Storages/IStorageBroker.Players.cs
+++ NeXtStandardStack.Core.Api/Brokers/Storages/IStorageBroker.Players.cs
@@ -9,5 +9,6 @@ namespace NeXtStandardStack.Core.Api.Brokers.Storages
     {
         ValueTask<Player> InsertPlayerAsync(Player player);
         IQueryable<Player> SelectAllPlayers();
+        ValueTask<Player> SelectPlayerByIdAsync(Guid playerId);
     }
 }
