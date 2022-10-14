a1ba6571a0c764b56c8ee82a7cdd7eddc308ba91
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 01:40:36 +0200

    BROKERS: Logging - Log LogDebug

--- NeXtStandardStack.Core.Api/Brokers/Loggings/ILoggingBroker.cs
+++ NeXtStandardStack.Core.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -6,5 +6,6 @@ namespace NeXtStandardStack.Core.Api.Brokers.Loggings
     {
         void LogInformation(string message);
         void LogTrace(string message);
+        void LogDebug(string message);
     }
 }
