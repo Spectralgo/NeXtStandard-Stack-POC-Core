8c1c2f7829452327848de52d6697eb3c89f0a39a
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 02:10:26 +0200

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Exceptions.Add.cs
@@ -0,0 +1,60 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using NeXtStandardStack.Core.Api.Models.Players;
+using NeXtStandardStack.Core.Api.Models.Players.Exceptions;
+using Xunit;
+
+namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
+{
+    public partial class PlayerServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Player somePlayer = CreateRandomPlayer();
+            SqlException sqlException = GetSqlException();
+
+            var failedPlayerStorageException =
+                new FailedPlayerStorageException(sqlException);
+
+            var expectedPlayerDependencyException =
+                new PlayerDependencyException(failedPlayerStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Player> addPlayerTask =
+                this.playerService.AddPlayerAsync(somePlayer);
+
+            PlayerDependencyException actualPlayerDependencyException =
+                await Assert.ThrowsAsync<PlayerDependencyException>(
+                    addPlayerTask.AsTask);
+
+            // then
+            actualPlayerDependencyException.Should()
+                .BeEquivalentTo(expectedPlayerDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPlayerAsync(It.IsAny<Player>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPlayerDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.cs
+++ NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
 using Moq;
 using NeXtStandardStack.Core.Api.Brokers.DateTimes;
 using NeXtStandardStack.Core.Api.Brokers.Loggings;
@@ -46,6 +48,9 @@ namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
             };
         }
 
+        private static SqlException GetSqlException() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
+
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 10).GetValue();
 
@@ -54,6 +59,9 @@ namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Player CreateRandomPlayer() =>
+            CreatePlayerFiller(dateTimeOffset: GetRandomDateTimeOffset()).Create();
 
         private static Player CreateRandomPlayer(DateTimeOffset dateTimeOffset) =>
             CreatePlayerFiller(dateTimeOffset).Create();
