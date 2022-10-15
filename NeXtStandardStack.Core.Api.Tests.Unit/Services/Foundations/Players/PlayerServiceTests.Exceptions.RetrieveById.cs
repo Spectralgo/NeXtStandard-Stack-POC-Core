1548c283d33f270e47ae1e6a45f9a4739d0e0c12
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 02:11:01 +0200

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using NeXtStandardStack.Core.Api.Models.Players;
+using NeXtStandardStack.Core.Api.Models.Players.Exceptions;
+using Xunit;
+
+namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
+{
+    public partial class PlayerServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedPlayerStorageException =
+                new FailedPlayerStorageException(sqlException);
+
+            var expectedPlayerDependencyException =
+                new PlayerDependencyException(failedPlayerStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPlayerByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Player> retrievePlayerByIdTask =
+                this.playerService.RetrievePlayerByIdAsync(someId);
+
+            PlayerDependencyException actualPlayerDependencyException =
+                await Assert.ThrowsAsync<PlayerDependencyException>(
+                    retrievePlayerByIdTask.AsTask);
+
+            // then
+            actualPlayerDependencyException.Should()
+                .BeEquivalentTo(expectedPlayerDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPlayerByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPlayerDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
