4f1fe2b83610631682b138f1904efb78bf1019e6
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 02:10:32 +0200

    ShouldThrowDependencyExceptionOnAddIfDatabaseUpdateErrorOccursAndLogItAsync -> FAIL

--- NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Exceptions.Add.cs
+++ NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using NeXtStandardStack.Core.Api.Models.Players;
 using NeXtStandardStack.Core.Api.Models.Players.Exceptions;
@@ -139,7 +140,8 @@ namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
                 await Assert.ThrowsAsync<PlayerDependencyValidationException>(
                     addPlayerTask.AsTask);
 
-            actualPlayerDependencyValidationException.Should().BeEquivalentTo(expectedPlayerValidationException);
+            actualPlayerDependencyValidationException.Should()
+                .BeEquivalentTo(expectedPlayerValidationException);
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTimeOffset(),
@@ -157,6 +159,55 @@ namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDatabaseUpdateErrorOccursAndLogItAsync()
+        {
+            // given
+            Player somePlayer = CreateRandomPlayer();
+
+            var databaseUpdateException =
+                new DbUpdateException();
+
+            var failedPlayerStorageException =
+                new FailedPlayerStorageException(databaseUpdateException);
+
+            var expectedPlayerDependencyException =
+                new PlayerDependencyException(failedPlayerStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(databaseUpdateException);
+
+            // when
+            ValueTask<Player> addPlayerTask =
+                this.playerService.AddPlayerAsync(somePlayer);
+
+            PlayerDependencyException actualPlayerDependencyException =
+                await Assert.ThrowsAsync<PlayerDependencyException>(
+                    addPlayerTask.AsTask);
+
+            // then
+            actualPlayerDependencyException.Should()
+                .BeEquivalentTo(expectedPlayerDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPlayerAsync(It.IsAny<Player>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPlayerDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
