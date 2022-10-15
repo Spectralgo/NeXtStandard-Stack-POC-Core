e932ba5ef5743814c391039a9f7c9c26051d1985
Florian Renard <56794425+Spectralgo@users.noreply.github.com>
2022-10-15 02:11:14 +0200

    ShouldThrowValidationExceptionOnModifyIfPlayerIsInvalidAndLogItAsync -> FAIL

--- NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Validations.Modify.cs
+++ NeXtStandardStack.Core.Api.Tests.Unit/Services/Foundations/Players/PlayerServiceTests.Validations.Modify.cs
@@ -48,5 +48,73 @@ namespace NeXtStandardStack.Core.Api.Tests.Unit.Services.Foundations.Players
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPlayerIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidPlayer = new Player
+            {
+                // TODO:  Add default values for your properties i.e. Name = invalidText
+            };
+
+            var invalidPlayerException = new InvalidPlayerException();
+
+            invalidPlayerException.AddData(
+                key: nameof(Player.Id),
+                values: "Id is required");
+
+            //invalidPlayerException.AddData(
+            //    key: nameof(Player.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Player model
+
+            invalidPlayerException.AddData(
+                key: nameof(Player.CreatedDate),
+                values: "Date is required");
+
+            invalidPlayerException.AddData(
+                key: nameof(Player.CreatedByUserId),
+                values: "Id is required");
+
+            invalidPlayerException.AddData(
+                key: nameof(Player.UpdatedDate),
+                values: "Date is required");
+
+            invalidPlayerException.AddData(
+                key: nameof(Player.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedPlayerValidationException =
+                new PlayerValidationException(invalidPlayerException);
+
+            // when
+            ValueTask<Player> modifyPlayerTask =
+                this.playerService.ModifyPlayerAsync(invalidPlayer);
+
+            PlayerValidationException actualPlayerValidationException =
+                await Assert.ThrowsAsync<PlayerValidationException>(
+                    modifyPlayerTask.AsTask);
+
+            //then
+            actualPlayerValidationException.Should().BeEquivalentTo(expectedPlayerValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPlayerValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePlayerAsync(It.IsAny<Player>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
